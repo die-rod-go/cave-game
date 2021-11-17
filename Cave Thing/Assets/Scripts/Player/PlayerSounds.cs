845cdf64381fb48bdc772e6ca65b76b5f5aaceef
trash-bag-replica <43896095+trash-bag-replica@users.noreply.github.com>
2021-11-17 03:44:06 -0500

    audio

---
+++ Cave Thing/Assets/Scripts/Player/PlayerSounds.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    //  walking sounds
+    public AudioClip[] runDirtSounds;
+    private AudioClip lastClip;
+
+    //  jumping sounds
+    public AudioClip jumpSound;
+
+    private AudioSource playerSoundSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSoundSource = GetComponent<AudioSource>();
+        lastClip = runDirtSounds[Random.Range(0, runDirtSounds.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void playFootstepSound()
+    {
+        do
+        {
+            playerSoundSource.clip = runDirtSounds[Random.Range(0, runDirtSounds.Length)];
+        } while (lastClip == playerSoundSource.clip);
+        lastClip = playerSoundSource.clip;
+        playerSoundSource.Play();
+    }
+
+    private void playJumpSound()
+    {
+        playerSoundSource.clip = jumpSound;
+        playerSoundSource.Play();
+    }
+}
